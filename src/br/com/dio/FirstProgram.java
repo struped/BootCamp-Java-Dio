887091d2153542e3ec894c68edb07a9dedcadcf0
struped <steimbach.estudo@gmail.com>
2023-02-22 12:29:25 -0300

    First Commit

---
+++ src/br/com/dio/FirstProgram.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+           /* double a = 5;
+            double b = 2;
+
+            System.out.println("Hello World! " + (a+b));*/
+    }
+}
