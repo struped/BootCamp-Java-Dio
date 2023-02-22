fa2ff5970d77606098665b03c04d259de0d0be41
struped <steimbach.estudo@gmail.com>
2023-02-22 09:48:01 -0300

    first commit

---
+++ src/JavaDIO/PrimeiroProgramaJava.java
@@ -0,0 +1,28 @@
+package JavaDIO;
+
+import JavaDIO.model.Gato;
+
+public class PrimeiroProgramaJava {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*
+		int a = 5;
+		int b = 5;
+		System.out.println("Hello World!" + (a+b));*/
+	}
+
+}
+
+class Livros {
+		private String nome;
+		private String npag;
+		
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Steim
+ *
+ */
+module primeiroprogramajava {
+}
